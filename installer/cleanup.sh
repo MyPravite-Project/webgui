acf11bee638d5c0cac22556cff47d23c3fe7965e
Scott Walters <scott@slowass.net>
2015-03-02 04:34:07 -0700

    cleanup.sh blowing away the mysql config too

--- installer/cleanup.sh
+++ installer/cleanup.sh
@@ -1,9 +1,11 @@
-echo "DANGER!  This will delete your WebGUI site and all of its data."
-echo "It's used for testing the installer.  Hit control-C now if you don't want to delete your database!"
+echo "DANGER!  This will delete your WebGUI site and all of its data, and your mysql config too, and probably other things."
+echo "It's used by the DEVELOPERS for testing the installer.  Hit control-C now if you don't want to delete your database!"
 echo "Hit enter to nuke everything."
 
 read line
 
+rm -f /etc/my.cnf
+rm -rf /etc/mysql
 rm -rf /data
 rm -rf /etc/nginx
 rm -f /etc/nginx/conf.d/webgui8.conf
